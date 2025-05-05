008b20563fa6cc563754dfb4ebf95509d6fb0779
MAURICIOANTUAN <mauriciosalinasboggioni@gmail.com>
2025-05-05 12:57:00 -0400

    Update and rename script-autoajuste (4).js to script-autoajuste.js

--- script-autoajuste.js
+++ script-autoajuste.js
@@ -1,12 +1,13 @@
-
 let datosOriginales = [];
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", iniciarApp);
+
+function iniciarApp() {
   Papa.parse('https://docs.google.com/spreadsheets/d/e/2PACX-1vS-urAZjllNC2T_Y5QK8SGwXHZ5KZwc1yxCOirfGqyaC0Tmyag92lKziYHHXoifB6Tibx6IfHC6QRVP/pub?gid=0&single=true&output=csv', {
     download: true,
     header: true,
-    complete: function(results) {
-      datosOriginales = results.data.filter(row => row['Contraparte'] || row['Ejercicio'] || row['Concepto']);
+    complete: function (results) {
+      datosOriginales = results.data;
       cargarFiltros();
       mostrarTabla(datosOriginales);
     }
@@ -15,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('btn-filtrar').addEventListener('click', filtrar);
   document.getElementById('btn-restablecer').addEventListener('click', restablecer);
   document.getElementById('btn-descargar').addEventListener('click', descargarExcel);
-});
+}
 
 function cargarFiltros() {
   const contraparteSet = new Set();
@@ -83,7 +84,7 @@ function mostrarTabla(data) {
     encabezado.forEach(col => {
       const celda = row[col] || '';
       const isLink = typeof celda === 'string' && celda.startsWith('http');
-      tbody += '<td>' + (isLink ? `<a href="${celda}" target="_blank">Ver documento</a>` : celda) + '</td>';
+      tbody += `<td>${isLink ? `<a href="${celda}" target="_blank">Ver documento</a>` : celda}</td>`;
     });
     tbody += '</tr>';
   });
@@ -94,6 +95,11 @@ function mostrarTabla(data) {
 
 function descargarExcel() {
   const tabla = document.getElementById('tabla-datos');
-  const wb = XLSX.utils.table_to_book(tabla, {sheet: "Resultados"});
+  const wb = XLSX.utils.table_to_book(tabla, { sheet: "Resultados" });
   XLSX.writeFile(wb, "contratos_estudios_autoajuste.xlsx");
 }
+
+function cerrarSesion() {
+  window.location.href = "index.html";
+}
+
